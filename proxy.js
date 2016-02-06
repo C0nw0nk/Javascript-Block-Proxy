e6e2f716803a50ec23ccd59168c2bf57616683e7
C0nw0nk <C0nw0nks@googlemail.com>
2016-02-06 19:36:53 +0000

    Create proxy.js

---
+++ proxy.js
@@ -0,0 +1,8 @@
+<script type="text/javascript"><!--//
+  /* Replace the domain with your own */
+  if(!location.hostname.match(/(^|\.)www.google.com$/)){
+    /* Redirect the person as an example. */
+    window.location="http://www.google.com/";
+  }
+  -->
+</script>
