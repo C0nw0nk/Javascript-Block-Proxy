11bf04fdc77329a8ba5b14843d078de4ae02b9b7
C0nw0nk <C0nw0nks@googlemail.com>
2016-02-06 19:38:22 +0000

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,5 +1,6 @@
 <script type="text/javascript"><!--//
-  /* Replace the domain with your own */
+  /* Replace the domain with your own 
+  If not the browsers hostname matches with your sites then */
   if(!location.hostname.match(/(^|\.)www.google.com$/)){
     /* Redirect the person as an example. */
     window.location="http://www.google.com/";
